83f5dee13d03be4e1c32c5ec2e2e898caf37512e
野島佑悟 <yugonojima@nojimayuusatorunoMacBook-Pro.local>
2020-08-27 03:30:00 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,47 +1,33 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = (props) => {
+  // const initialStates = {
+  //   name: "",
+  //   price: 1000,
+  // };
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment = () => {
-    setCount(count + 1);
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
   };
-  const increment2 = () => {
-    setCount((previousCount) => previousCount + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-  const decrement2 = () => {
-    setCount((previousCount) => previousCount - 1);
-  };
-  const countReset = () => setCount(0);
-  const countDouble = () => setCount(count * 2);
-  const countDividendThree = () =>
-    setCount(() => {
-      if (count % 3 === 0) {
-        const num = count / 3;
-        return num;
-      } else {
-        return count;
-      }
-    });
-
   return (
     <>
-      <div>count:{count}</div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={increment2}>+1</button>
-      <button onClick={decrement2}>-1</button>
-      <div>
-        <button onClick={countReset}>reset</button>
-        <button onClick={countDouble}>x2</button>
-        <button onClick={countDividendThree}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </>
   );
+};
+
+App.defaultProps = {
+  name: "",
+  price: 1000,
 };
 
 export default App;
