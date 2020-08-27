09ecc3a6016e617383fef6bf19fedfa2bb466b8a
野島佑悟 <yugonojima@nojimayuusatorunoMacBook-Pro.local>
2020-08-28 00:47:19 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import reducer from "../reducers";
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addEvent = (e) => {
+    e.preventDefault();
+
+    dispatch({
+      type: "CREATE_EVENT",
+      title,
+      body,
+    });
+    setTitle("");
+    setBody("");
+  };
+  console.log({ state });
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle"></input>
+          <input
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
         </div>
         <div className="form-group">
           <label htmlFor="formEventBody">ボディー</label>
-          <input className="form-control" id="formEventBody"></input>
+          <textarea
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
         </div>
 
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>
+          イベントを作成する
+        </button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
       </form>
 
@@ -27,8 +56,8 @@ const App = () => {
             <th>タイトル</th>
             <th>ボディー</th>
           </tr>
-          <tbody></tbody>
         </thead>
+        <tbody></tbody>
       </table>
     </div>
   );
